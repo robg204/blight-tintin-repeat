fedd894ba59b529fcf70b65b75aacc8afe352136
robg <robg@lush3.com>
2022-03-09 16:06:52 -0600

    debug

--- main.lua
+++ main.lua
@@ -10,7 +10,7 @@ else
 end
 
 function TTSCRprocess(matches)
-    blight.output("I am being passed a "..type(matches)..".  The value of position two is "..matches[2])
+    blight.output("I am being passed a "..type(matches)..".  The value of position two is "..matches[2] or "whoa nil")
     count = tonumber(matches[2])
     --for i = 0, count do
     --    mud.send(matches[3])
