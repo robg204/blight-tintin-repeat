48c9d02d450cf3486ae06bd836691d0723e262a9
robg <robg@lush3.com>
2022-03-09 16:08:32 -0600

    colour me embarassed

--- main.lua
+++ main.lua
@@ -10,13 +10,12 @@ else
 end
 
 function TTSCRprocess(matches)
-    blight.output("I am being passed a "..type(matches)..".  The value of position two is "..matches[2] or "whoa nil")
     count = tonumber(matches[2])
-    --for i = 0, count do
-    --    mud.send(matches[3])
-    --end
+    for i = 0, count do
+        mud.send(matches[3])
+    end
 end
 
-TTSCR['aliases']:add([==[^#\d+ .*$]==], TTSCRprocess)
+TTSCR['aliases']:add([==[^#(\d+) (.*$)]==], TTSCRprocess)
 
 blight.output("... --> tintin style completion loaded")
